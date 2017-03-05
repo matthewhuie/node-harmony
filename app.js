46fa7b83ed6d55910ed7c99bc1c59ad64f09ba6f
Matthew Huie <mhuie@foursquare.com>
2017-03-05 16:47:21 -0500

    Adding Express.js to handle endpoint

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var http = require('http');
+var express = require('express')
+var app = express()
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, world!');
-}).listen(3001);
+app.post('/harmonize', function (request, response) {
+  console.log('test')
+})
+
+app.listen(3000)
